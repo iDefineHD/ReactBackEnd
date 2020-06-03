8c447c3a51df795fd06bbb7167018e3069c4f6a0
Ashley <purpleizlegitx@gmail.com>
2020-06-03 16:27:22 +0100

    Added some auth. register and login.

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,72 @@
 const express = require('express')
-const router = express.Router();
+const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-//@route GET api/Auth 
+const User = require('../models/User')
+
+//@route GET api/Auth
 //@desc get Logged In User
 //@access Private
 
-router.get('/', (req, res)=>{
-    res.send('get Logged In User')
-})
+router.get(
+	'/',
+	[
+		check('email', 'Please include a valid email').isEmail(),
+		check('password', 'Please enter a password with the minimum characters of 6').exists(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
 
-//@route POST api/Auth 
+		const { email, password } = req.body
+
+		try {
+			let user = await User.findOne({ email })
+
+			if (!user) {
+				return res.status(400).json({ msg: 'Invalid Credentials' })
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password)
+
+			if (!isMatch) {
+				return res.status(400).json({ msg: 'Invalid Credentials' })
+			}
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			}
+
+			jwt.sign(
+				payload,
+				'Totally Not A Secret',
+				{
+					expiresIn: 360000, //3600 is an Hour
+				},
+				(err, token) => {
+					if (err) throw err
+					res.json({ token })
+				},
+			)
+		} catch (err) {
+			console.log(err)
+			return res.status(500).json({ msg: 'Server Error' })
+		}
+	},
+)
+
+//@route POST api/Auth
 //@desc Auth User & Get Token
 //@access Public
 
-router.post('/', (req, res)=>{
-    res.send('Register a User')
+router.post('/', (req, res) => {
+	res.send('Register a User')
 })
 
-module.exports = router;
-
+module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router();
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/User')
 
@@ -7,8 +10,53 @@ const User = require('../models/User')
 //@desc register user
 //@access public
 
-router.post('/', (req, res)=>{
-    res.send('registers user')
+router.post('/', [
+    check('name', 'name is required')
+    .not()
+    .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with the minimum characters of 6').isLength({ min: 6 })
+], async (req, res)=>{
+   const errors = validationResult(req)
+   if(!errors.isEmpty()) {
+       return res.status(400).json({errors: errors.array()})
+   }
+
+   const { name, email, password} = req.body;
+
+   try {
+       let user = await User.findOne({email})
+       if(user) {
+        return res.status(400).json({msg: "User Already Exists"})
+       }
+
+       user = new User({
+           name,
+           email,
+           password
+       })
+
+       const salt = await bcrypt.genSalt(10);
+       user.password = await bcrypt.hash(password, salt);
+       await user.save();
+
+       const payload = {
+           user: {
+               id: user.id
+           }
+       }
+
+       jwt.sign(payload, 'Totally Not A Secret', {
+           expiresIn: 360000, //3600 is an Hour
+       }, (err, token)=> {
+           if(err) throw err;
+           res.json({ token })
+       })
+
+   } catch (err) {
+       console.error(err.message);
+       res.status(500).send('Server Error')
+   }
 })
 
 module.exports = router;
