eec99fcb8f06afba444425b50ad6173cf9a110e8
Ashley <purpleizlegitx@gmail.com>
2020-06-17 15:43:48 +0100

    Updated contact, Now has delete, edit and clear current functionality

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react'
-import uuid from 'uuid'
+import {v4 as uuid} from "uuid"; 
 import ContactContext from './contactContext'
 import contactReducer from './contactReducer'
 import {
@@ -23,31 +23,62 @@ const ContactState = (props) => {
 				type: 'professional',
 			},
 			{
-				id: 1,
+				id: 2,
 				name: 'Jen Johnson',
 				email: 'Jen@gmail.com',
 				phone: '07914667927',
 				type: 'personal',
 			},
 			{
-				id: 1,
+				id: 3,
 				name: 'John Johnson',
 				email: 'John@gmail.com',
 				phone: '07914667957',
 				type: 'personal',
 			},
 		],
+		current: null
 	}
 
 	const [ state, dispatch ] = useReducer(contactReducer, initialState)
 
 	// Add Contact
 
+	const addContact = contact => {
+		contact.id = uuid
+		dispatch({
+			type: ADD_CONTACT,
+			payload: contact
+		})
+	}
+
 	// Delete Contact
+
+	const deleteContact = id => {
+		dispatch({
+			type: DELETE_CONTACT,
+			payload: id
+		})
+	}
 
 	//Set Current Contact
 
+	const setCurrent = contact => {
+		dispatch({
+			type: SET_CURRENT,
+			payload: contact
+		})
+	}
+
 	//Clear Current Contact
+
+	const clearCurrent = contact => {
+		dispatch({
+			type: CLEAR_CURRENT,
+			payload: contact
+		})
+	}
+
 
 	//Update Current Contact
 
@@ -59,6 +90,11 @@ const ContactState = (props) => {
 		<ContactContext.Provider
 			value={{
 				contacts: state.contacts,
+				current: state.current,
+				addContact,
+				deleteContact,
+				setCurrent,
+				clearCurrent
 			}}>
 			{props.children}
 		</ContactContext.Provider>
