a97295364254168250763b2f5c239dc78cc6aaac
Ashley <purpleizlegitx@gmail.com>
2020-06-22 16:46:37 +0100

    Final Commit, For Deploy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app)
 const port = process.env.PORT || 5000;
+const path = require('path')
 const db = require('./lib/db')
 
 //Initialize DB
@@ -10,14 +11,20 @@ db();
 // Initialize Middleware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res)=> {
-    res.json({Your:"#blacklivesmatter"})
-})
 
 //Define Routes
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/contacts', require('./routes/contacts'))
 app.use('/api/auth', require('./routes/auth'))
+
+// Serve Static Assets In Production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 http.listen(port, () => console.log(`Listening On Port ${port}`));
---
+++ client/build/precache-manifest.af044bb939942ffa2bef931ebfc6a28a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcd0c1254e9448771dfa23eeccb24ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "258237df68abe8976224",
+    "url": "/static/css/main.63832079.chunk.css"
+  },
+  {
+    "revision": "9119e0635652fc23a209",
+    "url": "/static/js/2.d8b9a2d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d8b9a2d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "258237df68abe8976224",
+    "url": "/static/js/main.d112613b.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
